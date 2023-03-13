f034082b0b5b4994ab59fc33ca92f2beca2a73a3
sbelman <sophie.belman@gmail.com>
2023-03-13 16:21:33 +0000

    make output sensible

--- HumanMobility_RR/MCMC_model/RunModel/extract_LLs.sh
+++ HumanMobility_RR/MCMC_model/RunModel/extract_LLs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 variable="$(wc -l ${1} | cut -d' ' -f1)"
 newvar="$(expr $variable - 43)"
-sed -n "10,${newvar}p" ${1} | cut -d' ' -f2 >  ll
+sed -n "10,${newvar}p" ${1} | cut -d' ' -f2 > ${1}_LLs
