0e3e579b1dc6a540922cc7e3cf675d1912bfb3d2
sbelman <sophie.belman@gmail.com>
2023-03-13 16:30:56 +0000

    r

--- HumanMobility_RR/MCMC_model/RunModel/extract_LLs.sh
+++ HumanMobility_RR/MCMC_model/RunModel/extract_LLs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 variable="$(wc -l ${1} | cut -d' ' -f1)"
 newvar="$(expr $variable - 43)"
-sed -n "10,${newvar}p" ${1} | cut -d' ' -f2 > test
-echo ${test} > ${1}_LLs.txt
+sed -n "10,${newvar}p" ${1} | cut -d' ' -f2 > ${1}_LLs.txt
+# echo ${test} > ${1}_LLs.txt
