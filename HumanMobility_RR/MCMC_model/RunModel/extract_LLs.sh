517fb2438ad4b50bcdd8669d5eae614c803595ef
sbelman <sophie.belman@gmail.com>
2023-03-14 11:21:56 +0000

    lls

--- HumanMobility_RR/MCMC_model/RunModel/extract_LLs.sh
+++ HumanMobility_RR/MCMC_model/RunModel/extract_LLs.sh
@@ -4,4 +4,4 @@
 #sed -n "10,${newvar}p" ${1} | cut -d' ' -f2 | 
 #grep "-" | grep -v "--" > ${1}_LLs.txt
 
-grep "\[1\]" chain1_adjhum.o | tail -n +10 | cut -d' ' -f2 > ${1}_LLs.txt
+grep "\[1\]" ${1} | tail -n +10 | cut -d' ' -f2 > ${1}_LLs.txt
